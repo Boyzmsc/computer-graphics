34f4b7303e3eef221d43da3d986bcf2bcba4fe59
Boyzmsc <boyzmsc@kookmin.ac.kr>
2021-05-22 02:46:56 +0900

    Upload assimp

--- 05.Assimp/cpp/Object.cpp
+++ 05.Assimp/cpp/Object.cpp
@@ -1,15 +1,40 @@
 #include "Object.h"
 
-void Object::init_buffer_objects() 
+void Object::init_buffer_objects()
 {
     // TODO
+    std::vector<unsigned int> indices;
+    aiFace face;
+    for (int i = 0; i < mMesh->mNumFaces; i++)
+    {
+        face = mMesh->mFaces[i];
+        for (int j = 0; j < face.mNumIndices; j++)
+        {
+            indices.push_back(face.mIndices[j]);
+        }
+    }
+
+    glGenBuffers(1, &position_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+    glBufferData(GL_ARRAY_BUFFER, mMesh->mNumVertices * sizeof(aiVector3D), &mMesh->mVertices[0], GL_STATIC_DRAW);
+
+    glGenBuffers(1, &color_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+    glBufferData(GL_ARRAY_BUFFER, mMesh->mNumVertices * sizeof(aiColor4D), &mMesh->mColors[0][0], GL_STATIC_DRAW);
+
+    glGenBuffers(1, &index_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 }
 
 glm::mat4 Object::get_model_matrix()
 {
     glm::mat4 mat_model = glm::mat4(1.0f);
-    
+
     // TODO
+    mat_model = mat_model * glm::translate(glm::mat4(1.0f), Object::translate());
+    mat_model = mat_model * Object::rotate();
+    mat_model = mat_model * glm::scale(glm::mat4(1.0f), Object::scale());
 
     return mat_model;
 }
@@ -17,8 +42,29 @@ glm::mat4 Object::get_model_matrix()
 void Object::draw(int loc_a_position, int loc_a_color)
 {
     // TODO
+    // 앞으로 언급하는 배열 버퍼(GL_ARRAY_BUFFER)는 position_buffer로 지정
+    glBindBuffer(GL_ARRAY_BUFFER, position_buffer);
+    // 버텍스 쉐이더의 attribute 중 a_position 부분 활성화
+    glEnableVertexAttribArray(loc_a_position);
+    // 현재 배열 버퍼에 있는 데이터를 버텍스 쉐이더 a_position에 해당하는 attribute와 연결
+    glVertexAttribPointer(loc_a_position, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
+
+    // 앞으로 언급하는 배열 버퍼(GL_ARRAY_BUFFER)는 color_buffer로 지정
+    glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+    // 버텍스 쉐이더의 attribute 중 a_color 부분 활성화
+    glEnableVertexAttribArray(loc_a_color);
+    // 현재 배열 버퍼에 있는 데이터를 버텍스 쉐이더 a_color에 해당하는 attribute와 연결
+    glVertexAttribPointer(loc_a_color, 4, GL_FLOAT, GL_FALSE, 0, (void *)0);
+
+    // IBO를 이용해 물체 그리기
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+    glDrawElements(GL_TRIANGLES, mMesh->mNumFaces * mMesh->mFaces->mNumIndices, GL_UNSIGNED_INT, (void *)0);
+
+    // 정점 attribute 배열 비활성화
+    glDisableVertexAttribArray(loc_a_position);
+    glDisableVertexAttribArray(loc_a_color);
 }
-    
+
 void Object::print_info()
 {
     std::cout << "print mesh info" << std::endl;
--- 05.Assimp/cpp/main.cpp
+++ 05.Assimp/cpp/main.cpp
@@ -32,33 +32,33 @@
 ////////////////////////////////////////////////////////////////////////////////
 /// initialization 관련 변수 및 함수
 ////////////////////////////////////////////////////////////////////////////////
-GLFWwindow* createWindow(int width, int height, const char* title);
-void init_window(GLFWwindow* window);
-bool init_scene_from_file(const std::string& filename);
+GLFWwindow *createWindow(int width, int height, const char *title);
+void init_window(GLFWwindow *window);
+bool init_scene_from_file(const std::string &filename);
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
 /// 쉐이더 관련 변수 및 함수
 ////////////////////////////////////////////////////////////////////////////////
-GLuint  program;          // 쉐이더 프로그램 객체의 레퍼런스 값
-GLint   loc_a_position;   // attribute 변수 a_position 위치
-GLint   loc_a_color;      // attribute 변수 a_color 위치
+GLuint program;       // 쉐이더 프로그램 객체의 레퍼런스 값
+GLint loc_a_position; // attribute 변수 a_position 위치
+GLint loc_a_color;    // attribute 변수 a_color 위치
 
-GLint   loc_u_PVM;        // uniform 변수 u_PVM 위치
+GLint loc_u_PVM; // uniform 변수 u_PVM 위치
 
-GLuint  position_buffer;  // GPU 메모리에서 position_buffer의 위치
-GLuint  color_buffer;     // GPU 메모리에서 color_buffer의 위치
-GLuint  index_buffer;     // GPU 메모리에서 index_buffer의 위치
+GLuint position_buffer; // GPU 메모리에서 position_buffer의 위치
+GLuint color_buffer;    // GPU 메모리에서 color_buffer의 위치
+GLuint index_buffer;    // GPU 메모리에서 index_buffer의 위치
 
-GLuint create_shader_from_file(const std::string& filename, GLuint shader_type);
+GLuint create_shader_from_file(const std::string &filename, GLuint shader_type);
 void init_shader_program();
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
 /// 변환 관련 변수 및 함수
 ////////////////////////////////////////////////////////////////////////////////
-glm::mat4     mat_model, mat_view, mat_proj;
-glm::mat4     mat_PVM;
+glm::mat4 mat_model, mat_view, mat_proj;
+glm::mat4 mat_PVM;
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -67,20 +67,20 @@ glm::mat4     mat_PVM;
 std::vector<Camera> cameras;
 
 float g_aspect = 1.0f;
-bool  g_is_perspective = true;
+bool g_is_perspective = true;
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
 /// 렌더링 관련 변수 및 함수
 ////////////////////////////////////////////////////////////////////////////////
-const aiScene* scene;
+const aiScene *scene;
 std::vector<Object> objects;
 std::vector<std::string> object_names;
 
-bool load_asset(const std::string& filename);
+bool load_asset(const std::string &filename);
 // void init_buffer_objects();     // VBO init 함수: GPU의 VBO를 초기화하는 함수.
-void render_object();           // rendering 함수: 물체(삼각형)를 렌더링하는 함수.
-void render(GLFWwindow* window);
+void render_object(); // rendering 함수: 물체(삼각형)를 렌더링하는 함수.
+void render(GLFWwindow *window);
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -89,23 +89,22 @@ void render(GLFWwindow* window);
 int obj_select_idx = 0;
 int cam_select_idx = 0;
 
-void init_imgui(GLFWwindow* window);
-void compose_imgui_frame(GLFWwindow* window, int key, int scancode, int action, int mods);
+void init_imgui(GLFWwindow *window);
+void compose_imgui_frame(GLFWwindow *window, int key, int scancode, int action, int mods);
 
 void key_callback();
-void scroll_callback(GLFWwindow* window, double x, double y);
+void scroll_callback(GLFWwindow *window, double x, double y);
 ////////////////////////////////////////////////////////////////////////////////
-
 
 ////////////////////////////////////////////////////////////////////////////////
 /// ImGuIZMO 관련 변수 및 함수
 ////////////////////////////////////////////////////////////////////////////////
-glm::quat qRot = quat(1.f, 0.f, 0.f, 0.f); 
+glm::quat qRot = quat(1.f, 0.f, 0.f, 0.f);
 ////////////////////////////////////////////////////////////////////////////////
 
-GLFWwindow* createWindow(int width, int height, const char* title)
+GLFWwindow *createWindow(int width, int height, const char *title)
 {
-  GLFWwindow* window; // create window
+  GLFWwindow *window; // create window
 
   // Initialize GLFW Library
   if (!glfwInit())
@@ -120,7 +119,7 @@ GLFWwindow* createWindow(int width, int height, const char* title)
   }
 
   // Make the current OpenGL contexts as one in the window
-  glfwMakeContextCurrent(window); 
+  glfwMakeContextCurrent(window);
 
   // Initialize GLEW library
   if (glewInit() != GLEW_OK)
@@ -134,20 +133,20 @@ GLFWwindow* createWindow(int width, int height, const char* title)
   return window;
 }
 
-void scroll_callback(GLFWwindow* window, double x, double y)
+void scroll_callback(GLFWwindow *window, double x, double y)
 {
   float Fovy = cameras[cam_select_idx].fovy();
-  Fovy += y;
+  Fovy -= y;
   cameras[cam_select_idx].set_fovy(Fovy);
 }
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
   glm::vec3 translate = objects[obj_select_idx].translate();
   glm::vec3 scale = objects[obj_select_idx].scale();
-  
+
   // move left
-  if (key == GLFW_KEY_H && action == GLFW_PRESS) 
+  if (key == GLFW_KEY_H && action == GLFW_PRESS)
     translate[0] -= 0.1f;
   // mode right
   if (key == GLFW_KEY_L && action == GLFW_PRESS)
@@ -155,7 +154,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
   // move up
   if (key == GLFW_KEY_K && action == GLFW_PRESS)
     translate[1] += 0.1f;
-  // move down 
+  // move down
   if (key == GLFW_KEY_J && action == GLFW_PRESS)
     translate[1] -= 0.1f;
 
@@ -164,22 +163,22 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     scale += 0.1f;
   if (key == GLFW_KEY_MINUS && action == GLFW_PRESS)
     scale -= 0.1f;
-  
+
   objects[obj_select_idx].set_translate(translate);
   objects[obj_select_idx].set_scale(scale);
 
   // camera extrinsic parameter
   if (key == GLFW_KEY_A && action == GLFW_PRESS)
     cameras[cam_select_idx].move_left(0.1f);
-  if (key == GLFW_KEY_D && action ==GLFW_PRESS)
-  cameras[cam_select_idx].move_right(0.1f);
+  if (key == GLFW_KEY_D && action == GLFW_PRESS)
+    cameras[cam_select_idx].move_right(0.1f);
   if (key == GLFW_KEY_W && action == GLFW_PRESS)
     cameras[cam_select_idx].move_forward(0.1f);
   if (key == GLFW_KEY_S && action == GLFW_PRESS)
     cameras[cam_select_idx].move_backward(0.1f);
 }
 
-void init_window(GLFWwindow* window) 
+void init_window(GLFWwindow *window)
 {
   init_imgui(window);
   init_shader_program();
@@ -192,10 +191,10 @@ void init_window(GLFWwindow* window)
   glfwSetScrollCallback(window, scroll_callback);
 }
 
-bool init_scene_from_file(const std::string& filename)
+bool init_scene_from_file(const std::string &filename)
 {
   std::ifstream fin(filename);
-  if (fin.fail()) 
+  if (fin.fail())
     return false;
 
   int count;
@@ -207,7 +206,7 @@ bool init_scene_from_file(const std::string& filename)
 
     fin >> name;
     if (!load_asset(name))
-    { 
+    {
       std::cout << "Failed to load a asset file: " << name << std::endl;
       return -1;
     }
@@ -227,32 +226,32 @@ bool init_scene_from_file(const std::string& filename)
     float front_x, front_y, front_z;
     float up_x, up_y, up_z;
 
-    fin >> pos_x >> pos_y >> pos_z >> 
-      front_x >> front_y >> front_z >>
-        up_x >> up_y >> up_z ;
+    fin >> pos_x >> pos_y >> pos_z >>
+        front_x >> front_y >> front_z >>
+        up_x >> up_y >> up_z;
 
     Camera camera(
-      glm::vec3(pos_x, pos_y, pos_z),
-      glm::vec3(front_x, front_y, front_z),
-      glm::vec3(up_x, up_y, up_z),
-      45.0f,
-      0.0f,
-      0.0f
-    );
+        glm::vec3(pos_x, pos_y, pos_z),
+        glm::vec3(front_x, front_y, front_z),
+        glm::vec3(up_x, up_y, up_z),
+        45.0f,
+        0.0f,
+        0.0f);
     cameras.push_back(camera);
   }
 
   return true;
 }
 
-void init_imgui(GLFWwindow* window) 
+void init_imgui(GLFWwindow *window)
 {
-  const char* glsl_version = "#version 120";
+  const char *glsl_version = "#version 120";
 
   // Setup Dear ImGui context
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
-  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  ImGuiIO &io = ImGui::GetIO();
+  (void)io;
   //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
   //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
 
@@ -264,13 +263,12 @@ void init_imgui(GLFWwindow* window)
   ImGui_ImplOpenGL3_Init(glsl_version);
 }
 
-
-bool load_asset(const std::string& filename)
-{  
-  const aiScene* curr_scene = aiImportFile(filename.c_str(), aiProcessPreset_TargetRealtime_MaxQuality);
+bool load_asset(const std::string &filename)
+{
+  const aiScene *curr_scene = aiImportFile(filename.c_str(), aiProcessPreset_TargetRealtime_MaxQuality);
   if (curr_scene != NULL)
   {
-    for (int i = 0; i < curr_scene->mNumMeshes; ++i) 
+    for (int i = 0; i < curr_scene->mNumMeshes; ++i)
     {
       Object obj;
 
@@ -282,7 +280,7 @@ bool load_asset(const std::string& filename)
 
     return true;
   }
-  else 
+  else
     return false;
 }
 
@@ -297,26 +295,25 @@ void compose_imgui_frame()
   {
     ImGui::Begin("model control");
 
-    
     for (int i = 0; i < object_names.size(); i++)
     {
       ImGui::RadioButton(object_names[i].c_str(), &obj_select_idx, i);
     }
-    
+
     glm::vec3 translate = objects[obj_select_idx].translate();
     glm::vec3 scale = objects[obj_select_idx].scale();
 
     if (ImGui::SliderFloat3("tranlsate", glm::value_ptr(translate), -10.0f, 10.0f))
       objects[obj_select_idx].set_translate(translate);
-    
+
     if (ImGui::SliderFloat3("scale", glm::value_ptr(scale), 0.0f, 1.5f))
       objects[obj_select_idx].set_scale(scale);
 
-    if(ImGui::gizmo3D("rotation", qRot))
+    if (ImGui::gizmo3D("rotation", qRot))
     {
       objects[obj_select_idx].set_rotate(glm::mat4_cast(qRot));
     }
-    
+
     ImGui::End();
   }
 
@@ -327,7 +324,7 @@ void compose_imgui_frame()
     ImGui::RadioButton("camera 0", &cam_select_idx, 0);
     ImGui::RadioButton("camera 1", &cam_select_idx, 1);
     ImGui::Checkbox("perspective", &g_is_perspective);
-    
+
     ImGui::Text("view direction");
     vec3 dir = vec3(cameras[cam_select_idx].front_direction());
     if (ImGui::gizmo3D("##gizmo2", dir, 100))
@@ -340,7 +337,7 @@ void compose_imgui_frame()
 }
 
 // GLSL 파일을 읽어서 컴파일한 후 쉐이더 객체를 생성하는 함수
-GLuint create_shader_from_file(const std::string& filename, GLuint shader_type)
+GLuint create_shader_from_file(const std::string &filename, GLuint shader_type)
 {
   GLuint shader = 0;
 
@@ -350,20 +347,20 @@ GLuint create_shader_from_file(const std::string& filename, GLuint shader_type)
   std::string shader_string;
 
   shader_string.assign(
-    (std::istreambuf_iterator<char>(shader_file)),
-    std::istreambuf_iterator<char>());
+      (std::istreambuf_iterator<char>(shader_file)),
+      std::istreambuf_iterator<char>());
 
   // Get rid of BOM in the head of shader_string
   // Because, some GLSL compiler (e.g., Mesa Shader compiler) cannot handle UTF-8 with BOM
-  if (shader_string.compare(0, 3, "\xEF\xBB\xBF") == 0)  // Is the file marked as UTF-8?
+  if (shader_string.compare(0, 3, "\xEF\xBB\xBF") == 0) // Is the file marked as UTF-8?
   {
     std::cout << "Shader code (" << filename << ") is written in UTF-8 with BOM" << std::endl;
     std::cout << "  When we pass the shader code to GLSL compiler, we temporarily get rid of BOM" << std::endl;
-    shader_string.erase(0, 3);                  // Now get rid of the BOM.
+    shader_string.erase(0, 3); // Now get rid of the BOM.
   }
 
-  const GLchar* shader_src = shader_string.c_str();
-  glShaderSource(shader, 1, (const GLchar * *)& shader_src, NULL);
+  const GLchar *shader_src = shader_string.c_str();
+  glShaderSource(shader, 1, (const GLchar **)&shader_src, NULL);
   glCompileShader(shader);
 
   GLint is_compiled;
@@ -390,14 +387,12 @@ GLuint create_shader_from_file(const std::string& filename, GLuint shader_type)
 // vertex shader와 fragment shader를 링크시켜 program을 생성하는 함수
 void init_shader_program()
 {
-  GLuint vertex_shader
-    = create_shader_from_file("./shader/vertex.glsl", GL_VERTEX_SHADER);
+  GLuint vertex_shader = create_shader_from_file("./shader/vertex.glsl", GL_VERTEX_SHADER);
 
   std::cout << "vertex_shader id: " << vertex_shader << std::endl;
   assert(vertex_shader != 0);
 
-  GLuint fragment_shader
-    = create_shader_from_file("./shader/fragment.glsl", GL_FRAGMENT_SHADER);
+  GLuint fragment_shader = create_shader_from_file("./shader/fragment.glsl", GL_FRAGMENT_SHADER);
 
   std::cout << "fragment_shader id: " << fragment_shader << std::endl;
   assert(fragment_shader != 0);
@@ -437,23 +432,42 @@ void init_shader_program()
 void render_object()
 {
   // TODO : set transform
-  mat_view = glm::mat4(1.0f); 
+  mat_view = glm::mat4(1.0f);
   mat_proj = glm::mat4(1.0f);
+  mat_model = glm::mat4(1.0f);
+
+  if (g_is_perspective)
+  {
+    cameras[cam_select_idx].set_mode(cameras[cam_select_idx].kPerspective);
+    mat_proj = glm::perspective(glm::radians(cameras[cam_select_idx].fovy()), 1.0f, 0.001f, 100.0f);
+  }
+  else
+  {
+    cameras[cam_select_idx].set_mode(cameras[cam_select_idx].kOrtho);
+    mat_proj = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, -10.0f, 10.0f);
+  }
+
+  mat_view = cameras[cam_select_idx].get_view_matrix();
 
   // 특정 쉐이더 프로그램 사용
   glUseProgram(program);
 
-  
   for (int i = 0; i < objects.size(); ++i)
   {
     // TODO : draw each object
+    mat_model = objects[i].get_model_matrix();
+
+    mat_PVM = mat_proj * mat_view * mat_model;
+    glUniformMatrix4fv(loc_u_PVM, 1, GL_FALSE, glm::value_ptr(mat_PVM));
+
+    objects[i].draw(loc_a_position, loc_a_color);
   }
 
   // 쉐이더 프로그램 사용해제
   glUseProgram(0);
 }
 
-void render(GLFWwindow* window) 
+void render(GLFWwindow *window)
 {
   glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -473,11 +487,10 @@ void render(GLFWwindow* window)
   glfwPollEvents();
 }
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
   // create window
-  GLFWwindow* window = createWindow(1000, 1000, "Hello Assimp");
+  GLFWwindow *window = createWindow(1000, 1000, "Hello Assimp");
 
   // initialize window
   init_window(window);
